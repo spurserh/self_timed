358a33e80b509a20dd2526b1e3919244cdb8d9d6
Sean Purser-Haskell <spurserh@wanglans-MacBook-Air.local>
2019-06-23 12:52:07 -0700

    Refactor to module + arduino control

--- ring_osc/example.v
+++ ring_osc/example.v
@@ -10,33 +10,39 @@ module top (
 );
 
 
-// ring oscillator - 213Mhz!
-wire chain_in, chain_out;
+wire chain_out;
 
-wire buffers_out;
-assign chain_out = buffers_out;
-//wire enable = 1; // arduino7
-//assign chain_in = enable ? !chain_out : 0;
-assign chain_in = !chain_out;
-
-SB_LUT4 #(
-	.LUT_INIT(16'd2)
-) buffers (
-	.O(buffers_out),
-	.I0(chain_in),
-	.I1(1'b0),
-	.I2(1'b0),
-	.I3(1'b0)
-);
+ring_oscillator osc(arduino7, chain_out);
 
 reg [27:0] counter = 0;
 always @(posedge chain_out) begin
 	counter <= counter + 1;
 end
 
-assign LED1 = counter[27];
+assign LED1 = counter[25];
 
 assign auxOut = counter[4];
 
 endmodule
+
+// 213Mhz!
+module ring_oscillator(input enable, output chain_out);
+	wire chain_in;
+
+	//wire enable = 1; // arduino7
+	assign chain_in = enable ? !chain_out : 0;
+	//assign chain_in = !chain_out;
+
+	SB_LUT4 #(
+		.LUT_INIT(16'd2)
+	) buffer (
+		.O(chain_out),
+		.I0(chain_in),
+		.I1(1'b0),
+		.I2(1'b0),
+		.I3(1'b0)
+	);
+endmodule
+
+
 
