25ed1a1568f0114cafca6632ffd8f86b22ecdb10
Sean Purser-Haskell <spurserh@wanglans-MacBook-Air.local>
2019-06-23 12:40:23 -0700

    Faster ring oscillator

--- ring_osc/example.v
+++ ring_osc/example.v
@@ -5,35 +5,38 @@ module top (
 	output LED3,
 	output LED4,
 	output LED5,
-	input arduino7
+	input arduino7,
+	output auxOut
 );
 
-wire enable = 1; // arduino7
 
-// ring oscillator
+// ring oscillator - 213Mhz!
 wire chain_in, chain_out;
 
-wire [99:0] buffers_in, buffers_out;
-assign buffers_in = {buffers_out[98:0], chain_in};
-assign chain_out = buffers_out[99];
-assign chain_in = enable ? !chain_out : 0;
+wire buffers_out;
+assign chain_out = buffers_out;
+//wire enable = 1; // arduino7
+//assign chain_in = enable ? !chain_out : 0;
+assign chain_in = !chain_out;
 
 SB_LUT4 #(
 	.LUT_INIT(16'd2)
-) buffers [99:0] (
+) buffers (
 	.O(buffers_out),
-	.I0(buffers_in),
+	.I0(chain_in),
 	.I1(1'b0),
 	.I2(1'b0),
 	.I3(1'b0)
 );
 
-reg [20:0] counter = 0;
+reg [27:0] counter = 0;
 always @(posedge chain_out) begin
 	counter <= counter + 1;
 end
 
-assign LED1 = counter[20];
+assign LED1 = counter[27];
+
+assign auxOut = counter[4];
 
 endmodule
 
