e64591d718a22a602ebd50fb2b8510fb0d0bb28f
Sean Purser-Haskell <spurserh@wanglans-MacBook-Air.local>
2019-06-23 12:55:44 -0700

    Cleanup

--- ring_osc/example.v
+++ ring_osc/example.v
@@ -8,30 +8,22 @@ module top (
 	input arduino7,
 	output auxOut
 );
+	wire osc_out;
+	ring_oscillator osc(!arduino7, osc_out);
 
+	reg [27:0] counter = 0;
+	always @(posedge osc_out) begin
+		counter <= counter + 1;
+	end
 
-wire chain_out;
-
-ring_oscillator osc(arduino7, chain_out);
-
-reg [27:0] counter = 0;
-always @(posedge chain_out) begin
-	counter <= counter + 1;
-end
-
-assign LED1 = counter[25];
-
-assign auxOut = counter[4];
-
+	assign LED1 = counter[25];
+	assign auxOut = counter[4];
 endmodule
 
-// 213Mhz!
+// >200Mhz!
 module ring_oscillator(input enable, output chain_out);
 	wire chain_in;
-
-	//wire enable = 1; // arduino7
 	assign chain_in = enable ? !chain_out : 0;
-	//assign chain_in = !chain_out;
 
 	SB_LUT4 #(
 		.LUT_INIT(16'd2)
